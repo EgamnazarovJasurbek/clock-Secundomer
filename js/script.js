f83877c250f4c3f7d5fc88dfe43b4ee6ae347b9b
EgamnazarovJasurbek <egamnazarovjasur@mail.ru>
2023-04-17 12:41:06 +0500

    Clock end

--- js/script.js
+++ js/script.js
@@ -1,85 +1,86 @@
 // ============================ CLOCK ====================================
 
-// let hours = document.querySelector('.hours')
-// let minutes = document.querySelector('.minutes')
+let hours = document.querySelector('.hours')
+let minutes = document.querySelector('.minutes')
 
-// let h = document.querySelector('.h')
-// let m = document.querySelector('.m')
-// let s = document.querySelector('.s')
+let h = document.querySelector('.h')
+let m = document.querySelector('.m')
+let s = document.querySelector('.s')
 
-// // console.log(new Date().getHours()) // Kompni Vaqtini ob beradi
+// console.log(new Date().getHours()) // Kompni Vaqtini ob beradi
 
-// let stop;
-// let x = 1
-// function rec() {
-//     console.log(x);
+// let stop
+let x = 1
+function rec() {
+    // console.log(x);
 
-//    // if(x < 10) {
-//    //     x++
-//    //     rec()
-//   // }
+   // if(x < 10) {
+   //     x++
+   //     rec()
+  // }
 
-//    stop =  setTimeout(() => {
-//         x++
-//         rec()
-//         if(x == 5 ) {
-//             clearInterval(stop)
-//         }
-//     }, 1000);
-// }
-// rec()
+  let stop =  setTimeout(() => {
+        x++
+        rec()
+        if(x == 5 ) {
+            clearInterval(stop)
+        }
+    }, 1000);
+}
+rec()
+
 // =======================================================================
 
-// function clock() {
+function clock() {
 
-// let time = new Date();
-// let hour = new Date().getHours();
-// let minut = new Date().getMinutes();
-// let sec = new Date().getSeconds();
+let time = new Date();
+let hour = new Date().getHours();
+let minut = new Date().getMinutes();
+let sec = new Date().getSeconds();
 
-// hours.innerText = hour
-// minutes.innerText = minut
+hours.innerText = hour
+minutes.innerText = minut
 
-// if(minut < 10) {
-//     minutes.innerText = "0" + minut
-// } else {
-//     minutes.innerText = minut
-// }
-// if(hour < 10) {
-//     hours.innerText = "0" + hour
-// } else {
-//     hours.innerText = hour
-// }
+if(minut < 10) {
+    minutes.innerText = "0" + minut
+} else {
+    minutes.innerText = minut
+}
+if(hour < 10) {
+    hours.innerText = "0" + hour
+} else {
+    hours.innerText = hour
+}
 
-// s.style.transform = `rotate(${sec * 6}deg)`
-// m.style.transform = `rotate(${minut * 6}deg)`
-// h.style.transform = `rotate(${hour * 8}deg)`
-
-
-//  setTimeout(() => {
-//     clock()
-//  }, 1000);
+s.style.transform = `rotate(${sec * 6}deg)`
+m.style.transform = `rotate(${minut * 6}deg)`
+h.style.transform = `rotate(${hour * 8}deg)`
 
 
-// }
-// clock()
+ setTimeout(() => {
+    clock()
+ }, 1000);
+
+
+}
+clock()
 // ========================================================================
 
-// let tabsItemActive = document.querySelector('.tabsItem.active')
+let tabsItemActive = document.querySelector('.tabsItem.active')
 
 // console.log(tabsItemActive);
 
-// tabsItemActive.addEventListener('click',function() {
-//     if(this.classList.contains('active')) {    // classList.contains() tekshiradi bor yoqligini
-//         console.log('bor');
+tabsItemActive.addEventListener('click',function() {
+    if(this.classList.contains('active')) {    // classList.contains() tekshiradi bor yoqligini
+        console.log('bor');
 
-//         // this.classList.add('aaa')
-//         // this.classList.remove('active')
-//         // this.classList.toggle('adasd')
-//     } else {
-//         console.log('yoq')
-//     }
-// })
+        // this.classList.add('aaa')
+        // this.classList.remove('active')
+        // this.classList.toggle('adasd')
+    } else {
+        console.log('yoq')
+    }
+})
 // ======================================Home Work=======================================
 
 let tabsItem = document.querySelectorAll('.tabsItem')
